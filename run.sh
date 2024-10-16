2608f3e3c559bb7c933f3ebafdc02fed4c8c4349
benjaniah <ZEPHANIAHPATTERSON@GMAIL.COM>
2024-10-15 17:21:19 -0700

    none

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 11DONTOPEN.txt --continue 74c2b --keyspace 74c2b000000000000:+1000000000000 1BY8GQbn
+./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 1DONTOPEN.txt --continue 75042 --keyspace 75042910000000000:+6f0000000000 1BY8GQbn
