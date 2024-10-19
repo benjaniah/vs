168d2b24b36c34db3388502718837d490695c67f
benjaniah <ZEPHANIAHPATTERSON@GMAIL.COM>
2024-10-19 15:55:45 -0700

    none

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3 -o 1RANS.txt --continue 73BD77 --keyspace 73BD7700000000000:+100000000000 1BY8GQbn
+./vanitysearch -t 0 -gpu -gpuId 0,1,2,3 -o 1-72f94d.txt --continue 72F94D --keyspace 72F94D00000000000:+100000000000 1BY8GQbn
