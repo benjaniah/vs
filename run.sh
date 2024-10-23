5054d0dcc4f76dc969d1963f66bef09d1670139d
benjaniah <ZEPHANIAHPATTERSON@GMAIL.COM>
2024-10-22 23:46:12 -0700

    none

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 1..5935A5.txt --continue 1..5935A5 --keyspace 5935A500000000000:+100000000000 1BY8GQbn
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 1..59F4F5.txt --continue 1..59F4F5 --keyspace 59F4F500000000000:+100000000000 1BY8GQbn
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 1..58878D.txt --continue 1..58878D --keyspace 58878D00000000000:+100000000000 1BY8GQbn
-./vanitysearch -t 0 -gpu -gpuId 0,1,2,3,4,5,6,7 -o 1..69FDAA.txt --continue 1..69FDAA --keyspace 69FDAA00000000000:+100000000000 1BY8GQbn
+
